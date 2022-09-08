37f4bae4ed4205fdb519b467428bbff87e9f9877
Somraj Saha <somraj.1994@gmail.com>
2022-09-08 18:19:35 +0530

    :poop: Add some Next UI :poop:

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import { AppProps } from "next/app";
 
+import { NextUIProvider } from "@nextui-org/react";
+
 function RootApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <NextUIProvider>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  );
 }
 
 export default RootApp;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,22 +1,28 @@
-import { Head, Html, Main, NextScript } from "next/document";
+import Document, { Head, Html, Main, NextScript } from "next/document";
 
-import { getCssText } from "../stitches.config";
+import { CssBaseline } from "@nextui-org/react";
+import React from "react";
 
-function RootDocument() {
-  return (
-    <Html lang="en">
-      <Head>
-        <style
-          id="stitches"
-          dangerouslySetInnerHTML={{ __html: getCssText() }}
-        />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return {
+      ...initialProps,
+      styles: React.Children.toArray([initialProps.styles]),
+    };
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>{CssBaseline.flush()}</Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
 
-export default RootDocument;
+export default MyDocument;
