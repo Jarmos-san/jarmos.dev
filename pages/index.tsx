5a6afd6c6f362bf2c41ae8104aa732c8520b8439
Somraj Saha <somraj.1994@gmail.com>
2022-09-09 17:03:27 +0530

    :poop: Create a bare minimum navigation bar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,39 @@
-import { Button } from "@nextui-org/react";
+import Layout from "../components/layout";
+import { Navbar, Text } from "@nextui-org/react";
 
 function HomePage() {
-  return <Button>Hello World!</Button>;
+  return (
+    <Layout>
+      <Navbar variant={"floating"} isBordered>
+        <Navbar.Brand>
+          <Text b color="inherit" hideIn="xs">
+            Somraj Saha
+          </Text>
+        </Navbar.Brand>
+
+        <Navbar.Content
+          hideIn="xs"
+          activeColor="default"
+          variant="underline-rounded"
+          enableCursorHighlight
+        >
+          <Navbar.Link href="#">Features</Navbar.Link>
+          <Navbar.Link href="#" isActive>
+            Customers
+          </Navbar.Link>
+          <Navbar.Link href="#">Pricing</Navbar.Link>
+          <Navbar.Link href="#">Company</Navbar.Link>
+        </Navbar.Content>
+
+        {/* <Switch */}
+        {/*   color="default" */}
+        {/*   iconOn={<SunIcon filled />} */}
+        {/*   iconOff={<MoonIcon filled />} */}
+        {/*   bordered */}
+        {/* /> */}
+      </Navbar>
+    </Layout>
+  );
 }
 
 export default HomePage;
