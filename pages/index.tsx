8aa3118ba309421ddf5cdc2ae1688a72e0a934c9
Somraj Saha <somraj.1994@gmail.com>
2022-09-15 18:12:57 +0530

    :sparkles: Add a bare minimum navigation bar to the project

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,38 +1,19 @@
-import Layout from "../components/layout";
 import { Navbar, Text } from "@nextui-org/react";
 
 function HomePage() {
   return (
-    <Layout>
-      <Navbar variant={"floating"} isBordered>
+    <>
+      <Navbar>
         <Navbar.Brand>
-          <Text b color="inherit" hideIn="xs">
-            Somraj Saha
-          </Text>
+          <Text>Somraj Saha</Text>
         </Navbar.Brand>
 
-        <Navbar.Content
-          hideIn="xs"
-          activeColor="default"
-          variant="underline-rounded"
-          enableCursorHighlight
-        >
-          <Navbar.Link href="#">Features</Navbar.Link>
-          <Navbar.Link href="#" isActive>
-            Customers
-          </Navbar.Link>
-          <Navbar.Link href="#">Pricing</Navbar.Link>
-          <Navbar.Link href="#">Company</Navbar.Link>
+        <Navbar.Content>
+          <Navbar.Link href="/">Home</Navbar.Link>
+          <Navbar.Link href="/about">About</Navbar.Link>
         </Navbar.Content>
-
-        {/* <Switch */}
-        {/*   color="default" */}
-        {/*   iconOn={<SunIcon filled />} */}
-        {/*   iconOff={<MoonIcon filled />} */}
-        {/*   bordered */}
-        {/* /> */}
       </Navbar>
-    </Layout>
+    </>
   );
 }
 
