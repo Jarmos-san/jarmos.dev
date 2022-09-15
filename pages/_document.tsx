7ceb1e4146ba565db2ec934429ae50587635cd36
Somraj Saha <somraj.1994@gmail.com>
2022-09-15 18:13:42 +0530

    :label: Add some Type Aliases to make TypeScript happy

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,16 @@
-import Document, { Head, Html, Main, NextScript } from "next/document";
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from "next/document";
 
 import { CssBaseline } from "@nextui-org/react";
 import React from "react";
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
+  static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
     return {
       ...initialProps,
