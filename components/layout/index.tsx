ec00be87140489de238992b9a5bb1db085a0499b
Somraj Saha <somraj.1994@gmail.com>
2022-09-15 18:14:01 +0530

    :poop: Remove some redundacy temporarily

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,12 +1,7 @@
-import { styled } from "@nextui-org/react";
 import { PropsWithChildren } from "react";
 
-const Box = styled("main", {
-  boxSizing: "border-box",
-});
-
 function Layout({ children }: PropsWithChildren) {
-  return <Box>{children}</Box>;
+  return <>{children}</>;
 }
 
 export default Layout;
