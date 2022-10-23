dc0d65fc098587a99ac57a17b2afac87fb812b85
Somraj Saha <somraj.1994@gmail.com>
2022-10-23 20:22:45 +0530

    :poop: Add some temporary code for experimentation

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,23 +1,24 @@
 import NextDocument, { Head, Html, Main, NextScript } from "next/document";
+
 import { getCssText } from "../stitches.config";
 
 class RootDocument extends NextDocument {
-  render() {
-    return (
-      <Html lang="en">
-        <Head>
-          <style
-            id="stitches"
-            dangerouslySetInnerHTML={{ __html: getCssText() }}
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+    render() {
+        return (
+            <Html lang="en">
+                <Head>
+                    <style
+                        id="stitches"
+                        dangerouslySetInnerHTML={{ __html: getCssText() }}
+                    />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
 }
 
 export default RootDocument;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,26 @@
 import { styled } from "../stitches.config";
 
+// Base container element which every other component inherits from.
+const Container = styled("div", {
+    display: "flex",
+});
+
 /* TODO: Add some "drop shadow" to the component as suggested in the template. */
-const Heading = styled("h1", {
-    backgroundImage: "linear-gradient($glow10, $glow20)",
-    backgroundClip: "text",
-    "-webkit-text-fill-color": "transparent",
+const Heading = styled(Container, {
+    // backgroundImage: "linear-gradient($glow10, $glow20)",
+    // backgroundClip: "text",
+    // "-webkit-text-fill-color": "transparent",
+    justifyContent: "center",
 });
 
 function HomePage() {
     return (
-        <Heading>I&apos;m Somraj, Software Engineer And Technical Writer.</Heading>
+        <>
+            <title>Somraj Saha</title>
+            <Heading as="h1">
+                I&apos;m Somraj, Software Engineer And Technical Writer.
+            </Heading>
+        </>
     );
 }
 
