ec321f2a0727474da29af4c60d08602645254810
Somraj Saha <somraj.1994@gmail.com>
2022-10-25 21:11:43 +0530

    :poop: Add some more stuff to learn from

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,42 @@ function HomePage() {
     return (
         <>
             <title>Somraj Saha</title>
-            <p>Hello World!</p>
+
+            <header>
+                <h1>
+                    This is Somraj Saha, a Full-Stack Software Engineer & Technical
+                    Writer.
+                </h1>
+                <p>
+                    I specialize in rapidly prototyping web applications & related
+                    software. During my free time I share my knowledge & experience with
+                    the rest of the world. You can find my FOSS contributions on my{" "}
+                    <a
+                        href="https://github.com/Jarmos-san"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        GitHub
+                    </a>{" "}
+                    profile & my social media presence on{" "}
+                    <a
+                        href="https://twitter.com/Jarmosan"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        Twitter
+                    </a>{" "}
+                    as well{" "}
+                    <a
+                        href="https://www.linkedin.com/in/jarmos"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        LinkedIn
+                    </a>
+                    .
+                </p>
+            </header>
         </>
     );
 }
