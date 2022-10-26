f5b690cc20a88c9732de7876eed97c148a1d9017
Somraj Saha <somraj.1994@gmail.com>
2022-10-26 13:03:49 +0530

    :sparkles: Create a seperate SCSS Module for the header section

---
+++ components/header/index.tsx
@@ -0,0 +1,38 @@
+import styles from "./header.module.scss";
+
+function Header() {
+    return (
+        <header className={styles.headerClass}>
+            <h1>
+                This is Somraj Saha, a Full-Stack Software Engineer & Technical Writer.
+            </h1>
+            <p>
+                I specialize in rapidly prototyping web applications & related software.
+                During my free time I share my knowledge & experience with the rest of
+                the world. You can find my FOSS contributions on my{" "}
+                <a
+                    href="https://github.com/Jarmos-san"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    GitHub
+                </a>{" "}
+                profile & my social media presence on{" "}
+                <a href="https://twitter.com/Jarmosan" target="_blank" rel="noreferrer">
+                    Twitter
+                </a>{" "}
+                as well{" "}
+                <a
+                    href="https://www.linkedin.com/in/jarmos"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    LinkedIn
+                </a>
+                .
+            </p>
+        </header>
+    );
+}
+
+export default Header;
