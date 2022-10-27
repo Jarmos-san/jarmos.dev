67cd36f42ffaa7a320cf996444111743ea974183
Somraj Saha <somraj.1994@gmail.com>
2022-10-27 17:50:56 +0530

    :poop: Experiment with a base `container` class

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,11 @@ import Header from "../components/header";
 
 function HomePage() {
     return (
-        <>
+        <div className="container">
             <title>Somraj Saha</title>
 
             <Header />
-        </>
+        </div>
     );
 }
 
