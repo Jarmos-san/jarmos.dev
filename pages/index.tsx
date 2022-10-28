fcdb2b0dd50600cccda2742f8b8f844eb695cc5f
Somraj Saha <somraj.1994@gmail.com>
2022-10-28 17:39:45 +0530

    :sparkles: Add a cta section to the index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
+import CtaSection from "../components/cta";
 import Header from "../components/header";
 
 function HomePage() {
     return (
         <div className="container">
             <title>Somraj Saha</title>
-
             <Header />
+            <CtaSection />
         </div>
     );
 }
