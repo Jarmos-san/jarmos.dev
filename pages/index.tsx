ca315f8e09858b0dd3d765791039fab5bfb9400e
Somraj Saha <somraj.1994@gmail.com>
2022-10-30 23:26:28 +0530

    :recycle: Move some common components to the `layout` component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,13 @@
 import CtaSection from "../components/cta";
-import Footer from "../components/footer";
 import Header from "../components/header";
-import Navbar from "../components/navbar";
 
 function HomePage() {
     return (
         <>
             <title>Somraj Saha</title>
             <div className="container">
-                <Navbar />
                 <Header />
                 <CtaSection />
-                <Footer />
             </div>
         </>
     );
