2a9dd877aff777389687ebcb12984be5a7984847
Somraj Saha <somraj.1994@gmail.com>
2022-10-30 16:45:49 +0530

    :sparkles: Add a new "about" page for the future

---
+++ pages/about/index.tsx
@@ -0,0 +1,12 @@
+import styles from "./about.module.scss";
+
+function AboutPage() {
+    return (
+        <>
+            <title>Somraj Saha | About Me</title>
+            <div className={styles.container}>Hello World!</div>
+        </>
+    );
+}
+
+export default AboutPage;
