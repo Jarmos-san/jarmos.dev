e52284850de1d30d5716a1f9d797de339aedebdd
Somraj Saha <somraj.1994@gmail.com>
2022-10-30 23:23:54 +0530

    :recycle: Add a "layout" component to the root

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
 import { AppProps } from "next/app";
 
+import Layout from "../components/layout";
 import "../styles/main.scss";
 
 function RootApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <Layout>
+            <Component {...pageProps} />
+        </Layout>
+    );
 }
 
 export default RootApp;
