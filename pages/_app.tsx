608bdd95819720ddb8558084f64e2057c37b7c17
Somraj Saha <somraj.1994@gmail.com>
2022-10-30 16:34:38 +0530

    :poop: Remove some redundant comments from previous experiments

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,8 @@
 import { AppProps } from "next/app";
 
-// import { resetCSS } from "../stitches.config";
 import "../styles/main.scss";
 
 function RootApp({ Component, pageProps }: AppProps) {
-    // resetCSS();
     return <Component {...pageProps} />;
 }
 
