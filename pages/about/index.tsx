05942d63eb33f32c08827eea9b5c0a0b3cc4074e
Somraj Saha <somraj.1994@gmail.com>
2022-10-31 16:10:45 +0530

    :sparkles: Add an "about" page

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -4,7 +4,100 @@ function AboutPage() {
     return (
         <>
             <title>Somraj Saha | About Me</title>
-            <div className={styles.container}>Hello World!</div>
+            <main className={styles.container}>
+                <article>
+                    <h1>About Somraj Saha</h1>
+                    <p>Hi There!</p>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
+                        luctus metus at lectus luctus interdum. Nam finibus urna posuere
+                        neque malesuada ultrices. Lorem ipsum dolor sit amet, consectetur
+                        adipiscing elit. Aliquam faucibus sem ut tellus tempus luctus.
+                        Suspendisse ultricies pharetra ligula, at vehicula lacus aliquam
+                        dictum. Vestibulum ante ipsum primis in faucibus orci luctus et
+                        ultrices posuere cubilia curae; Nullam luctus lacus eu auctor
+                        consectetur. Pellentesque efficitur aliquet purus. Fusce bibendum
+                        sodales nisi sed consequat. Praesent egestas sed ante a lobortis.
+                    </p>
+                    <p>
+                        Phasellus et nunc quam. Morbi vehicula, nunc eget tempus volutpat,
+                        dolor mauris ultrices turpis, ac scelerisque nunc quam in ex. Donec
+                        lacinia ligula vitae urna laoreet accumsan. Morbi eget vulputate
+                        risus. Donec convallis at ante ultricies vehicula. Vivamus mauris
+                        nisl, iaculis eu aliquam vitae, viverra et nibh. Maecenas sagittis
+                        dui ut nibh dictum, at aliquam nisl iaculis. Nulla ut velit non sem
+                        elementum laoreet nec volutpat neque. Maecenas ut quam eget elit
+                        efficitur dapibus vestibulum ac urna. Praesent lacus risus, viverra
+                        eu porttitor eget, hendrerit sed dolor. Sed placerat, sapien
+                        sagittis accumsan luctus, velit diam mattis libero, sed accumsan
+                        odio dui eget nisi.
+                    </p>
+                    <p>
+                        Sed pharetra et nulla sit amet consectetur. Nam eleifend mollis nunc
+                        ut cursus. Nulla facilisi. Donec dignissim, arcu sed auctor
+                        sollicitudin, mauris orci facilisis mi, in elementum ante libero sit
+                        amet erat. Sed egestas volutpat gravida. Phasellus semper
+                        consectetur dolor quis efficitur. Aenean faucibus ut ante non
+                        facilisis. Duis sagittis gravida turpis, vel placerat turpis dapibus
+                        scelerisque. Donec posuere cursus efficitur. Nulla facilisi.
+                        Curabitur id efficitur quam. Suspendisse vitae nibh nec turpis
+                        laoreet consequat et eu est. Nullam at aliquet felis, a auctor
+                        tellus. Praesent euismod tortor congue felis convallis ultrices.
+                    </p>
+                    <h2>About My Tool Belt</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
+                        luctus metus at lectus luctus interdum. Nam finibus urna posuere
+                        neque malesuada ultrices. Lorem ipsum dolor sit amet, consectetur
+                        adipiscing elit. Aliquam faucibus sem ut tellus tempus luctus.
+                        Suspendisse ultricies pharetra ligula, at vehicula lacus aliquam
+                        dictum. Vestibulum ante ipsum primis in faucibus orci luctus et
+                        ultrices posuere cubilia curae; Nullam luctus lacus eu auctor
+                        consectetur. Pellentesque efficitur aliquet purus. Fusce bibendum
+                        sodales nisi sed consequat. Praesent egestas sed ante a lobortis.
+                    </p>
+                    <p>
+                        Phasellus et nunc quam. Morbi vehicula, nunc eget tempus volutpat,
+                        dolor mauris ultrices turpis, ac scelerisque nunc quam in ex. Donec
+                        lacinia ligula vitae urna laoreet accumsan. Morbi eget vulputate
+                        risus. Donec convallis at ante ultricies vehicula. Vivamus mauris
+                        nisl, iaculis eu aliquam vitae, viverra et nibh. Maecenas sagittis
+                        dui ut nibh dictum, at aliquam nisl iaculis. Nulla ut velit non sem
+                        elementum laoreet nec volutpat neque. Maecenas ut quam eget elit
+                        efficitur dapibus vestibulum ac urna. Praesent lacus risus, viverra
+                        eu porttitor eget, hendrerit sed dolor. Sed placerat, sapien
+                        sagittis accumsan luctus, velit diam mattis libero, sed accumsan
+                        odio dui eget nisi.
+                    </p>
+                    <p>
+                        Sed pharetra et nulla sit amet consectetur. Nam eleifend mollis nunc
+                        ut cursus. Nulla facilisi. Donec dignissim, arcu sed auctor
+                        sollicitudin, mauris orci facilisis mi, in elementum ante libero sit
+                        amet erat. Sed egestas volutpat gravida. Phasellus semper
+                        consectetur dolor quis efficitur. Aenean faucibus ut ante non
+                        facilisis. Duis sagittis gravida turpis, vel placerat turpis dapibus
+                        scelerisque. Donec posuere cursus efficitur. Nulla facilisi.
+                        Curabitur id efficitur quam. Suspendisse vitae nibh nec turpis
+                        laoreet consequat et eu est. Nullam at aliquet felis, a auctor
+                        tellus. Praesent euismod tortor congue felis convallis ultrices.
+                    </p>
+                    <h2 id="contact">How to Contact Me</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non
+                        nisl at sem venenatis ultrices. Suspendisse sem urna, pharetra eu
+                        dignissim rutrum, efficitur ut ante. Integer pretium hendrerit
+                        sapien, eu gravida est pellentesque in. Sed luctus scelerisque lorem
+                        sit amet mattis. Phasellus ultrices vestibulum tempor. Donec a
+                        scelerisque urna. Aliquam ornare id quam sit amet sodales.
+                        Suspendisse imperdiet condimentum suscipit. Nullam auctor vehicula
+                        quam at dictum. Integer in eleifend ex. Donec et eros nec ex rutrum
+                        luctus. Praesent molestie neque libero, quis venenatis metus
+                        bibendum quis. Morbi dapibus quis nunc eu interdum. Proin semper
+                        sapien quis magna condimentum, quis eleifend est efficitur. Aenean
+                        sagittis ipsum nec turpis suscipit, sit amet dapibus tortor luctus.
+                    </p>
+                </article>
+            </main>
         </>
     );
 }
