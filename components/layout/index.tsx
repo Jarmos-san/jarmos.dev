481b5377fe34bbe93ad252ad2733beb2ca212429
Somraj Saha <somraj.1994@gmail.com>
2022-10-31 16:13:33 +0530

    :poop: Remove some redundancy from the layout component

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,13 +1,12 @@
 import Footer from "../footer";
 import Navbar from "../navbar";
-import styles from "./layout.module.scss";
 import { PropsWithChildren } from "react";
 
 function Layout({ children }: PropsWithChildren) {
     return (
         <>
             <Navbar />
-            <div className={styles.container}>{children}</div>
+            <div>{children}</div>
             <Footer />
         </>
     );
