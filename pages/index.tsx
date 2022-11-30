e5904c8e34b16735630e0ba707a8f70f39f15350
Somraj Saha <somraj.1994@gmail.com>
2022-11-30 15:53:12 +0530

    :poop: Add some temporary changes for future reference

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
 import CtaSection from "../components/cta";
 import Header from "../components/header";
 
+// import ProjectsSection from "../components/projects";
+
 function HomePage() {
     return (
         <>
-            <title>Somraj Saha</title>
+            {/* <title>Somraj Saha</title> */}
             <Header />
+            {/* <ProjectsSection /> */}
             <CtaSection />
         </>
     );
