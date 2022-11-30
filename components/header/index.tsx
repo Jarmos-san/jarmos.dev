b891e4039d92a545a22e28b71c3170cb50a76816
Somraj Saha <somraj.1994@gmail.com>
2022-11-30 15:51:48 +0530

    :poop: Update the header section

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -8,16 +8,17 @@ function Header() {
             </h1>
 
             <p className={styles.subheading}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sit amet
-                odio a nulla eleifend molestie. Nam non posuere nibh, ac dignissim
-                dolor. Nullam viverra ac massa vel euismod. Praesent vestibulum velit a
-                risus pellentesque, sed finibus lorem vulputate. Nulla vel eleifend mi.
-                Praesent consectetur vel odio non rutrum. Proin dictum orci est, eget
-                lobortis felis tristique at. Aenean quis erat orci. Curabitur nec quam
-                mattis ante rhoncus fermentum sit amet at urna. Sed velit purus,
-                pharetra sed massa ac, suscipit pellentesque erat. In hendrerit, velit
-                eget maximus viverra, tortor diam consequat dui, vitae auctor elit
-                lectus nec libero. Nam nec ultrices felis.
+                I work as a Software Engineer at{" "}
+                <a
+                    href="https://www.augwir.com"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                >
+                    Augwir Technologies
+                </a>{" "}
+                to build some awesome Augmented Reality (AR) products! You can find more
+                about me, my work, knowledge & experience on this blog. So, feel free to
+                check it out as per you convenience.
             </p>
         </header>
     );
