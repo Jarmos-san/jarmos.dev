468f1af86e5e3674194bb2d1b86a8b47482cc97a
Somraj Saha <somraj.1994@gmail.com>
2022-11-30 15:53:33 +0530

    :sparkles: Add a new component for the projects section

---
+++ components/projects/index.tsx
@@ -0,0 +1,11 @@
+import styles from "./projects.module.scss";
+
+function ProjectsSection() {
+    return (
+        <section className={styles.container}>
+            <h2>Projects</h2>
+        </section>
+    );
+}
+
+export default ProjectsSection;
