86e2efbfa876776f8ffdfbf386773443a73ddd8d
Somraj Saha <somraj.1994@gmail.com>
2022-12-21 17:06:37 +0530

    :hammer: Configure Next.js to skip the lint checks

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const path = require("path");
 
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
