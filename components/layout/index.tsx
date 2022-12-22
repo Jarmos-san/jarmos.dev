b0bfbb1affce3c5189e63403b5c434c9273b67e4
Somraj Saha <somraj.1994@gmail.com>
2022-12-22 16:25:34 +0530

    :beers: Add some shit I've no idea about

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,15 +1,16 @@
 import Footer from "../footer";
 import Navbar from "../navbar";
+import styles from "./layout.module.scss";
 import { PropsWithChildren } from "react";
 
 function Layout({ children }: PropsWithChildren) {
-  return (
-    <>
-      <Navbar />
-      {children}
-      <Footer />
-    </>
-  );
+    return (
+        <>
+            <Navbar />
+            <div className={styles.container}>{children}</div>
+            <Footer />
+        </>
+    );
 }
 
 export default Layout;
