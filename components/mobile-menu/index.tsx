6cd3add95bd21b72cc5d823b63919b71212613cc
Somraj Saha <somraj.1994@gmail.com>
2022-12-23 15:30:27 +0530

    :sparkles: Add a new updated `mobile-menu` component

---
+++ components/mobile-menu/index.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+import styles from "./mobile-menu.module.scss";
+
+function MobileMenu() {
+    return (
+        <div className={styles.mobileMenu}>
+            <ul>
+                <li>
+                    <Link href="/">Home</Link>
+                </li>
+
+                <li>
+                    <Link href="/about">About</Link>
+                </li>
+            </ul>
+        </div>
+    );
+}
+
+export default MobileMenu;
