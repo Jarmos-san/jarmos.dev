7a042c8ae1279764c71cd2daf9ce7243fdf71d8c
Somraj Saha <somraj.1994@gmail.com>
2022-12-23 15:39:29 +0530

    :recycle: Refactor the source code with a common layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,7 @@
-import Footer from "../components/footer";
-import Navbar from "../components/navbar";
-
 function HomePage() {
     return (
         <>
-            <Navbar />
             <h1>Hello World!</h1>
-            <Footer />
         </>
     );
 }
