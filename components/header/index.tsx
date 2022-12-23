2f2fc11709a79ebf1b5070f8fa9cb29b6359d78c
Somraj Saha <somraj.1994@gmail.com>
2022-12-23 21:47:24 +0530

    :sparkles: Add an updated header section as a component

---
+++ components/header/index.tsx
@@ -0,0 +1,28 @@
+import styles from "./header.module.scss";
+
+function HeaderSection() {
+    return (
+        <header className={styles.headerContainer}>
+            <h1 className={styles.heading}>
+                This is Somraj, a Software Engineer &amp; Technical Writer.
+            </h1>
+
+            <p className={styles.subheading}>
+                I work as a Senior Software Engineer at{" "}
+                <a
+                    href="https://www.augwir.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Augwir Technologies
+                </a>{" "}
+                where we are building the next-generation of some awesome Augmented
+                Reality (AR) products! You can find out more about me, my work,
+                knowledge & experience on this website. So, feel free to check it out as
+                per your convenience.
+            </p>
+        </header>
+    );
+}
+
+export default HeaderSection;
