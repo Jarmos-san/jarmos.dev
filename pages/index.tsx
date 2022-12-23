59fbce0ba02477ae43e5a4db361178e62f0bc2aa
Somraj Saha <somraj.1994@gmail.com>
2022-12-24 00:12:59 +0530

    :bento: Add the missing title for the page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import Head from "next/head";
+
 import CtaSection from "../components/cta";
 import HeaderSection from "../components/header";
 import ProjectSection from "../components/projects";
@@ -5,6 +7,9 @@ import ProjectSection from "../components/projects";
 function HomePage() {
     return (
         <>
+            <Head>
+                <title>Somraj Saha</title>
+            </Head>
             <HeaderSection />
             <ProjectSection />
             <CtaSection />
