9bd88da2220fb8b16aff769d60a9c7137011d719
Somraj Saha <somraj.1994@gmail.com>
2022-12-29 18:27:13 +0530

    :bug: Fix the broken OpenGraph images

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,50 +5,50 @@ import HeaderSection from "../components/header";
 import ProjectSection from "../components/projects";
 
 function HomePage() {
-  return (
-    <>
-      <Head>
-        <title>Somraj Saha</title>
-        <meta property="og:url" content="https://jarmos.vercel.app" />
-        <meta property="og:title" content="Somraj Saha" />
-        <meta
-          property="og:description"
-          content="Somraj Saha, a self-taught Senior Software Engineer & Technical Writer."
-        />
-        <meta
-          property="og:image"
-          content="https://jarmos.vercel.app/assets/logo-1200-630.png"
-        />
-        <meta
-          property="og:image:alt"
-          content="Profile picture of Somraj Saha"
-        />
-        <meta property="og:type" content="website" />
-        <meta property="og:locale" content="en_GB" />
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:domain" content="jarmos.vercel.app" />
-        <meta property="twitter:url" content="https://jarmos.vercel.app" />
-        <meta name="twitter:title" content="Somraj Saha" />
-        <meta
-          name="twitter:description"
-          content="Somraj Saha, a self-taught Senior Software Engineer & Technical Writer."
-        />
-        <meta
-          name="twitter:image"
-          content="https://jarmos.vercel.app/assets/profile.png"
-        />
-        <meta
-          property="twitter:image:alt"
-          content="Profile picture of Somraj Saha"
-        />
-        <meta name="twitter:site" content="@Jarmosan" />
-        <meta name="twitter:creator" content="@Jarmosan" />
-      </Head>
-      <HeaderSection />
-      <ProjectSection />
-      <CtaSection />
-    </>
-  );
+    return (
+        <>
+            <Head>
+                <title>Somraj Saha</title>
+                <meta property="og:url" content="https://jarmos.vercel.app" />
+                <meta property="og:title" content="Somraj Saha" />
+                <meta
+                    property="og:description"
+                    content="Somraj Saha, a self-taught Senior Software Engineer & Technical Writer."
+                />
+                <meta
+                    property="og:image"
+                    content="https://jarmos.vercel.app/assets/logo-1200-630.png"
+                />
+                <meta
+                    property="og:image:alt"
+                    content="Profile picture of Somraj Saha"
+                />
+                <meta property="og:type" content="website" />
+                <meta property="og:locale" content="en_GB" />
+                <meta property="twitter:card" content="summary_large_image" />
+                <meta property="twitter:domain" content="jarmos.vercel.app" />
+                <meta property="twitter:url" content="https://jarmos.vercel.app" />
+                <meta name="twitter:title" content="Somraj Saha" />
+                <meta
+                    name="twitter:description"
+                    content="Somraj Saha, a self-taught Senior Software Engineer & Technical Writer."
+                />
+                <meta
+                    name="twitter:image"
+                    content="https://jarmos.vercel.app/assets/logo-1200-630.png"
+                />
+                <meta
+                    property="twitter:image:alt"
+                    content="Profile picture of Somraj Saha"
+                />
+                <meta name="twitter:site" content="@Jarmosan" />
+                <meta name="twitter:creator" content="@Jarmosan" />
+            </Head>
+            <HeaderSection />
+            <ProjectSection />
+            <CtaSection />
+        </>
+    );
 }
 
 export default HomePage;
