f2157763f91512141eefe503ae6e915b42bbc150
Somraj Saha <somraj.1994@gmail.com>
2022-12-30 18:15:46 +0530

    :recycle: Refactor all the necessary SEO stuff

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,54 +1,21 @@
-import Head from "next/head";
-
 import CtaSection from "../components/cta";
 import HeaderSection from "../components/header";
 import ProjectSection from "../components/projects";
+import Seo from "../components/seo";
 
 function HomePage() {
-  return (
-    <>
-      <Head>
-        <title>Somraj Saha</title>
-        <meta property="og:url" content="https://jarmos.vercel.app" />
-        <meta property="og:title" content="Somraj Saha" />
-        <meta
-          property="og:description"
-          content="Somraj Saha, a self-taught Senior Software Engineer & Technical Writer."
-        />
-        <meta
-          property="og:image"
-          content="https://jarmos.vercel.app/assets/logo-1200-630.png"
-        />
-        <meta
-          property="og:image:alt"
-          content="Profile picture of Somraj Saha"
-        />
-        <meta property="og:type" content="website" />
-        <meta property="og:locale" content="en_GB" />
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:domain" content="jarmos.vercel.app" />
-        <meta property="twitter:url" content="https://jarmos.vercel.app" />
-        <meta name="twitter:title" content="Somraj Saha" />
-        <meta
-          name="twitter:description"
-          content="Somraj Saha, a self-taught Senior Software Engineer & Technical Writer."
-        />
-        <meta
-          name="twitter:image"
-          content="https://jarmos.vercel.app/assets/logo-1200-630.png"
-        />
-        <meta
-          property="twitter:image:alt"
-          content="Profile picture of Somraj Saha"
-        />
-        <meta name="twitter:site" content="@Jarmosan" />
-        <meta name="twitter:creator" content="@Jarmosan" />
-      </Head>
-      <HeaderSection />
-      <ProjectSection />
-      <CtaSection />
-    </>
-  );
+    return (
+        <>
+            <Seo
+                description="Somraj Saha, a self-taught Senior Software Engineer & Technical Writer."
+                imageHref="/assets/logo-1200-630.png"
+                imageAlt="Profile picture of the personal website."
+            />
+            <HeaderSection />
+            <ProjectSection />
+            <CtaSection />
+        </>
+    );
 }
 
 export default HomePage;
