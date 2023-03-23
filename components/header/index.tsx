8fbb4cada25118e25c4974a8d913770d038a83b0
Somraj Saha <somraj.1994@gmail.com>
2023-03-23 17:12:14 +0530

    :poop: Remove any mention of Augwir

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,25 +1,23 @@
+import Link from "next/link";
+
 import styles from "./header.module.scss";
 
 function HeaderSection() {
   return (
     <header className={styles.headerContainer}>
       <h1 className={styles.heading}>
-        This is Somraj, a Software Engineer &amp; Technical Writer.
+        This is Somraj, an ML Engineer &amp; Technical Writer.
       </h1>
 
       <p className={styles.subheading}>
-        I work as a Senior Software Engineer at{" "}
-        <a
-          href="https://www.augwir.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Augwir Technologies
-        </a>{" "}
-        where we are building the next-generation of some awesome Augmented
-        Reality (AR) products! You can find out more about me, my work,
-        knowledge & experience on this website. So, feel free to check it out as
-        per your convenience.
+        I work as an Machine Learning Consultant specialising in building,
+        designing & integrating ML techniques into custom software products
+        end-to-end. Want to learn more about me & my work? Then head over to the{" "}
+        <Link href="/about">About Me</Link> page or check out my{" "}
+        <a href="./resume.pdf" target="_blank">
+          resume
+        </a>
+        .
       </p>
     </header>
   );
