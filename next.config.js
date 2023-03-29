c0ee45d9be13eb49f0422ec01b39c9ca5c7128d2
Somraj Saha <somraj.1994@gmail.com>
2023-03-29 08:42:15 +0530

    :wrench: Add some security headers for the website

--- next.config.js
+++ next.config.js
@@ -1,6 +1,32 @@
 /** @type {import('next').NextConfig} */
 const path = require("path");
 
+const contentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self';
+  style-src 'self';
+  font-src 'self' https://fonts.googleapis.com;
+`;
+
+const securityHeaders = [
+  {
+    key: "Content-Security-Policy",
+    value: contentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
+  },
+  {
+    key: "X-Frame-Options",
+    value: "SAMEORIGIN",
+  },
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block",
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+];
+
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
@@ -8,6 +34,17 @@ const nextConfig = {
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
+  },
+  // Info about the security headers are available at:
+  // https://nextjs.org/docs/advanced-features/security-headers
+  async headers() {
+    return [
+      {
+        // Apply the security headers to all routes of the website
+        source: "/:path*",
+        headers: securityHeaders,
+      },
+    ];
   },
 };
 
