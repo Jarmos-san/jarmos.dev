e1a78071649a362c5f719eb21287b883698edcf4
Somraj Saha <somraj.1994@gmail.com>
2023-03-29 17:37:40 +0530

    :poop: Add a temporary comment for future reference

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,8 @@ import { AppProps } from "next/app";
 import Layout from "@components/layout";
 import "@styles/main.scss";
 
+// import '@styles/globals.css'
+
 function RootApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
