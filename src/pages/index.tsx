4fb47f2257b2b523a66daee96dec5b7b53c61485
Somraj Saha <somraj.1994@gmail.com>
2023-04-03 18:25:59 +0530

    :recycle: Recreate the navigation bar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,10 @@
-import { Navbar } from "flowbite-react";
-
-import Image from "next/image";
-import Link from "next/link";
-
-import { navlinks } from "@constants/navlinks";
+import Navbar from "@components/navbar";
 
 function HomePage() {
   return (
-    <Navbar fluid={true} className="container mx-auto">
-      <Navbar.Brand>
-        <Link href="/">
-          <Image
-            src="./assets/logo-og.svg"
-            alt="Logo for the website"
-            width={75}
-            height={75}
-          />
-        </Link>
-      </Navbar.Brand>
-      <Navbar.Toggle />
-      <Navbar.Collapse>
-        {navlinks.map((navlink) => (
-          <Link
-            key={navlink.label}
-            href={navlink.href}
-            passHref
-            className="mx-auto text-lg underline underline-offset-2
-            hover:underline-offset-4"
-          >
-            <Navbar.Link href={navlink.href}>{navlink.label}</Navbar.Link>
-          </Link>
-        ))}
-      </Navbar.Collapse>
-    </Navbar>
+    <>
+      <Navbar />
+    </>
   );
 }
 
