0fd9d94d399b0b11927b9487deefd873ee8aa00f
Somraj Saha <somraj.1994@gmail.com>
2023-04-04 13:47:10 +0530

    :sparkles: Add a layout component for all the pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
 import { AppProps } from "next/app";
 
+import LayoutComponent from "@components/layout";
 import "@styles/globals.css";
 
 function RootApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <LayoutComponent>
+      <Component {...pageProps} />
+    </LayoutComponent>
+  );
 }
 
 export default RootApp;
