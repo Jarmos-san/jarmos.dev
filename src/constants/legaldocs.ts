1d46f166951cff16c4f404344a6f691552a4bf7b
Somraj Saha <somraj.1994@gmail.com>
2023-04-04 15:49:31 +0530

    :bulb: Add links to all the legal docs

---
+++ src/constants/legaldocs.ts
@@ -0,0 +1,19 @@
+type legaldocTypes = {
+  label: string;
+  href: string;
+};
+
+export const legaldocs: legaldocTypes[] = [
+  {
+    label: "Terms of Usage",
+    href: "/terms-of-usage",
+  },
+  {
+    label: "Privacy Policy",
+    href: "privacy-policy",
+  },
+  {
+    label: "Distribution Rights",
+    href: "distribution-rights",
+  },
+];
