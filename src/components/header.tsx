ee775206807a7f5a99ce5ebed5dc3c55b3f28892
Somraj Saha <somraj.1994@gmail.com>
2023-04-13 17:20:54 +0530

    :recycle: Refactor the entire header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,19 +2,26 @@ import Link from "next/link";
 
 function Header() {
   return (
-    <header className="flex flex-col space-y-4 bg-blue-10 px-10 py-12">
-      <h1 className="text-3xl font-semibold text-green-10">
-        This is Somraj, an ML Engineer & Technical Writer
+    <header className="container mx-auto space-y-5 px-8 md:px-20 lg:px-36">
+      <h1
+        className="bg-gradient-to-r from-green-30 to-green-10 bg-clip-text
+        text-2xl font-bold leading-10 text-green-10 text-transparent md:text-3xl
+        lg:text-5xl"
+      >
+        I&apos;m Somraj, an Independent Software Engineer & a Writer.
       </h1>
 
-      <p className="text-white-10">
-        I work as a Machine Learning Consultant specialising in building,
-        designing & integrating ML practices into custom software products,
-        end-to-end.
+      <p className="text-white-10 lg:pr-32">
+        I work as an Independent Software Engineer consulting for startups &amp;
+        business owners to build custom end-to-end software. Whether your needs
+        are for an in-house team or a B2B/B2C setting, I&apos;ll take care of it
+        without worries! My specialisation is in Machine Learning &amp; it&apos;
+        related fields of application.
       </p>
 
-      <p className="text-white-10">
-        Want to learn more about me? Then head over to the &ldquo;
+      <p className="text-white-10 lg:pr-60">
+        That said, would you like to learn more about me? Then head over to the
+        &ldquo;
         <Link
           href="/about"
           className="text-green-30 underline underline-offset-2
@@ -22,16 +29,16 @@ function Header() {
         >
           About Me
         </Link>
-        &rdquo; page or check out my{" "}
+        &rdquo; page or check out my &ldquo;
         <a
           href="/resume.pdf"
           target="_blank"
           className="text-green-30 underline underline-offset-2
               hover:text-white-20"
         >
-          resume
-        </a>{" "}
-        for more information.
+          Resume
+        </a>
+        &rdquo; for more information.
       </p>
     </header>
   );
