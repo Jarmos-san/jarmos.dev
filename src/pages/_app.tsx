05c15640359e3dbfcfb66b9d2c98661ec682ec0e
Somraj Saha <somraj.1994@gmail.com>
2023-04-13 15:27:22 +0530

    :recycle: Refactor the way the layout is coded

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,20 @@
 import { AppProps } from "next/app";
 
-import LayoutComponent from "@components/layout";
+import Footer from "@components/footer";
+import Navbar from "@components/navbar";
 import "@styles/globals.css";
 
 function RootApp({ Component, pageProps }: AppProps) {
   return (
-    <LayoutComponent>
-      <Component {...pageProps} />
-    </LayoutComponent>
+    <div
+      className={`${poppins.variable} flex min-h-screen flex-col bg-blue-10`}
+    >
+      <Navbar />
+      <main className="flex-grow">
+        <Component {...pageProps} />
+      </main>
+      <Footer />
+    </div>
   );
 }
 
