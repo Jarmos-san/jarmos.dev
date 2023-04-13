6fcf993eecaba7ecf897697eef2c04856413cb11
Somraj Saha <somraj.1994@gmail.com>
2023-04-13 15:27:59 +0530

    :bento: Add the Poppins font to the website

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,16 @@
 import { AppProps } from "next/app";
+import { Poppins } from "next/font/google";
 
 import Footer from "@components/footer";
 import Navbar from "@components/navbar";
 import "@styles/globals.css";
+
+const poppins = Poppins({
+  weight: ["400", "500", "600", "700"],
+  display: "optional",
+  subsets: ["latin"],
+  variable: "--font-poppins",
+});
 
 function RootApp({ Component, pageProps }: AppProps) {
   return (
