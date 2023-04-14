ad98639ecc42e9e897494d57efaa7c23fc756e89
Somraj Saha <somraj.1994@gmail.com>
2023-04-14 18:00:49 +0530

    :sparkles: Add the portfolio section on the homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import CallToAction from "@components/cta";
 import Header from "@components/header";
+import ProjectSection from "@components/projects";
 
 function HomePage() {
   return (
     <>
       <Header />
+      <ProjectSection />
       <CallToAction />
     </>
   );
