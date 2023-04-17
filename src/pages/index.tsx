d4388bcbd506d069fb62a064c7cf175b00405f60
Somraj Saha <somraj.1994@gmail.com>
2023-04-17 15:32:27 +0530

    :sparkles: Add the SEO component to the landing page as well

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,18 @@
 import CallToAction from "@components/cta";
 import Header from "@components/header";
 import ProjectSection from "@components/projects";
+import Seo from "@components/seo";
 
 function HomePage() {
   return (
     <>
+      <Seo
+        desc="The personal website of Somraj Saha who is a Full-Stack Software
+        Engineer and is working independently to provide value services to
+        startup founders & business owners."
+        image="/assets/logo-1200-630.svg"
+        imageAlt="The logo of the personal website"
+      />
       <Header />
       <ProjectSection />
       <CallToAction />
