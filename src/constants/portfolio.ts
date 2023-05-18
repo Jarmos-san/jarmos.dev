8dea4301b38f2797094bc760eb2214dbd2c3cd0f
Somraj Saha <somraj.1994@gmail.com>
2023-05-18 14:55:57 +0530

    :sparkles: Add link to the Neovimulus project

--- src/constants/portfolio.ts
+++ src/constants/portfolio.ts
@@ -15,7 +15,7 @@ export const portfolio: PortfolioTypes[] = [
             manage my dotfiles. Jefer gives back more decision-making
             responsibilities to the user unlike its alternatives.`,
     border: "border-fuchsia-400",
-    href: "/jefer",
+    href: "https://github.com/Jarmos-san/jefer",
   },
   {
     label: "devsindia.in",
@@ -24,7 +24,7 @@ export const portfolio: PortfolioTypes[] = [
             the countless previous experimentations I performed to create an
             ideal personal website.`,
     border: "border-teal-400",
-    href: "/devsindia-in",
+    href: "https://github.com/developersIndia/website",
   },
   {
     label: "jarmos.dev",
@@ -32,7 +32,16 @@ export const portfolio: PortfolioTypes[] = [
             community. The project was created using Next.js, SASS & TypeScript.
             This project was created in hopes to gather more organic traffic.`,
     border: "border-rose-400",
-    href: "/personal-website",
+    href: "https://github.com/Jarmos-san/jarmos.dev",
+  },
+  {
+    label: "Neovimulus",
+    desc: `A no non-sense and minimal configuration framework for those who
+            wants to give Neovim a try without much fuss. Set up a complete
+            Neovim experience with LSP-based autocompletion and more by running
+            a single line of command!`,
+    border: "border-yellow-200",
+    href: "https://github.com/Jarmos-san/neovimulus",
   },
 ];
 
