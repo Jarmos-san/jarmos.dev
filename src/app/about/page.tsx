bd716fa47de73e45010b60a71c47738a41e8b175
Sagar Kapoor <69609200+Sagar-Kap@users.noreply.github.com>
2023-10-31 13:18:37 +0200

    fix: test commit to check code highlighting

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,51 +1,6 @@
 import { Metadata } from "next";
 import Link from "next/link";
 
-export const metadata: Metadata = {
-  title: "About",
-  description: `This is about Somraj Saha who is a Full-Stack Software Engineer and 
-  working as an Independent Software Developer. He works with startup founders and 
-  business owners to bring fresh ideas to production and serve paying customers.`,
-  alternates: {
-    canonical: "/about",
-  },
-  openGraph: {
-    title: "About | Somraj Saha",
-    description: `This is about Somraj Saha who is a Full-Stack Software Engineer and 
-    working as an Independent Software Developer. He works with startup founders and 
-    business owners to bring fresh ideas to production and serve paying customers.`,
-    url: "https://jarmos.dev/about",
-    siteName: "Somraj Saha",
-    images: [
-      {
-        url: "https://ik.imagekit.io/jarmos/tr:w-1200,h-627/logo-og.svg",
-        alt: "The logo of the personal website",
-      },
-    ],
-    type: "website",
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Somraj Saha | About",
-    description: `This is about Somraj Saha who is a Full-Stack Software Engineer and 
-    working as an Independent Software Developer. He works with startup founders and 
-    business owners to bring fresh ideas to production and serve paying customers.`,
-    creator: "@Jarmosan",
-    images: ["https://ik.imagekit.io/jarmos/tr:w-1200,h-627/logo-og.svg"],
-  },
-  appleWebApp: {
-    title: "About | Somraj Saha",
-    statusBarStyle: "black-translucent",
-    startupImage: [
-      "/apple-touch-icon.png",
-      {
-        url: "/apple-touch-icon.png",
-        media: "(device-width: 768px) and (device-height: 1024px)",
-      },
-    ],
-  },
-};
-
 const AboutPage = () => {
   return (
     <>
