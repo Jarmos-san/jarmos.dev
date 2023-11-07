2da3928123413ea6e2c1d42cd4ba90c501db85e8
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2023-11-07 22:37:41 +0530

    chore(refactor): add a few custom MDX components

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,7 +1,13 @@
+import Image from "next/image";
+
 import type { MDXComponents } from "mdx/types";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
+    h1: ({ children }) => (
+      <h1 className="text-extrabold text-3xl text-green-20">{children}</h1>
+    ),
+    img: (props) => <Image width={200} height={200} alt="" {...props} />,
     ...components,
   };
 }
