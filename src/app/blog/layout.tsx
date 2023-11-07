31bac3f74998fedbd250ba3f940fc6ecbd6e46a1
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2023-11-07 22:38:33 +0530

    chore(refactor): add a base text colour for the blog posts

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -4,7 +4,7 @@ const BlogLayout = ({ children }: PropsWithChildren) => {
   // The contents of all the blogs are rendered inside the "article" tag
   return (
     <article
-      className="container prose mx-auto max-w-screen-sm px-5
+      className="container mx-auto max-w-screen-sm px-5 text-white-10
       lg:max-w-screen-md xl:max-w-screen-lg 2xl:max-w-screen-xl"
     >
       {children}
