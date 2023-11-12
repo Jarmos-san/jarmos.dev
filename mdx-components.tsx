35ffd711f596aa46b6c3f7088779489fa88ebb88
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2023-11-12 15:50:07 +0530

    chore(refactor): apply some styles to the headers of the blogs

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -5,9 +5,11 @@ import type { MDXComponents } from "mdx/types";
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     h1: ({ children }) => (
-      <h1 className="text-extrabold text-3xl text-green-20">{children}</h1>
+      <h1 className="text-3xl font-bold text-green-30">{children}</h1>
     ),
-    img: (props) => <Image width={200} height={200} alt="" {...props} />,
+    img: (props) => (
+      <Image width={1200} height={640} alt="" className="my-10" {...props} />
+    ),
     ...components,
   };
 }
