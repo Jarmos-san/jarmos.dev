c23ee6fecc6aaa7c6ab8ef2e0f82048365aa8e69
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2023-12-14 21:39:46 +0530

    chore: remove some bs Terraform code

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,14 +11,6 @@ provider "vercel" {
   api_token = var.vercel_api_token
 }
 
-data "vercel_project" "astro_project" {
-  name = "astro-blog"
-}
-
-data "vercel_prebuilt_project" "astro_build_source" {
-  path = "../dist"
-}
-
 resource "vercel_project" "astro_blog" {
   name             = "astro-blog"
   framework        = "astro"
@@ -26,12 +18,6 @@ resource "vercel_project" "astro_blog" {
   dev_command      = "npm run dev"
   install_command  = "npm ci"
   output_directory = "dist"
-}
-
-resource "vercel_deployment" "astro_live_blog" {
-  project_id  = data.vercel_project.astro_project.id
-  files       = data.vercel_prebuilt_project.astro_build_source.output
-  path_prefix = data.vercel_prebuilt_project.astro_build_source.path
 }
 
 resource "vercel_project_domain" "astro_blog_domain" {
