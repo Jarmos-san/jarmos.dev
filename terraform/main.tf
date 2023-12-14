08f299e92a3c72878ffeaef0ca6db8d65f378e01
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2023-12-14 20:57:02 +0530

    chore: regenerate the Terraform project

---
+++ terraform/main.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    vercel = {
+      source  = "vercel/vercel"
+      version = "~> 1.0"
+    }
+  }
+}
+
+provider "vercel" {
+  api_token = var.vercel_api_token
+}
