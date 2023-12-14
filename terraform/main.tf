a881bcacb4b9c6b7191e3972334f70ffb0797443
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2023-12-14 21:31:03 +0530

    chore: attempt to create a Terraform project

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,3 +10,31 @@ terraform {
 provider "vercel" {
   api_token = var.vercel_api_token
 }
+
+data "vercel_project" "astro_project" {
+  name = "astro-blog"
+}
+
+data "vercel_prebuilt_project" "astro_build_source" {
+  path = "../dist"
+}
+
+resource "vercel_project" "astro_blog" {
+  name             = "astro-blog"
+  framework        = "astro"
+  build_command    = "npm run build"
+  dev_command      = "npm run dev"
+  install_command  = "npm ci"
+  output_directory = "dist"
+}
+
+resource "vercel_deployment" "astro_live_blog" {
+  project_id  = data.vercel_project.astro_project.id
+  files       = data.vercel_prebuilt_project.astro_build_source.output
+  path_prefix = data.vercel_prebuilt_project.astro_build_source.path
+}
+
+resource "vercel_project_domain" "astro_blog_domain" {
+  project_id = vercel_project.astro_blog.id
+  domain     = "astro-jarmos.vercel.app"
+}
