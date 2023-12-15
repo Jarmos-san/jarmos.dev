1dae1030f841a7a117e6d825e896624b118d5c0e
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2023-12-15 17:52:18 +0530

    chore: generate a config file for `commitlint`

---
+++ commitlint.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "header-max-length": [2, "always", 72],
+    "body-max-line-length": [1, "always", Infinity],
+  },
+};
