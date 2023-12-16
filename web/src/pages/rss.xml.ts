7c2e541f6beea2fbf4f35dd91f350c0e3df8e19e
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2023-12-16 17:03:06 +0530

    chore: add logic to generate an RSS feed

---
+++ web/src/pages/rss.xml.ts
@@ -0,0 +1,20 @@
+import { getCollection } from "astro:content";
+
+import rss from "@astrojs/rss";
+
+export async function GET(context) {
+  const blog = await getCollection("blog");
+  return rss({
+    title: "Somraj Saha’s Blog",
+    description:
+      "Somraj Saha's digital garden where he shares his knowledge, experience and learnings as a self-taught programmer.",
+    site: context.site,
+    stylesheet: "/rss/styles.xsl",
+    items: blog.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.publishedDate,
+      description: post.data.description,
+      link: `/blog/${post.slug}/`,
+    })),
+  });
+}
