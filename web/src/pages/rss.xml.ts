0e3be9306c1184a4fe2b4af1930001699093f6f5
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2023-12-18 16:46:25 +0530

    fix: update the types and make Astro happy

--- web/src/pages/rss.xml.ts
+++ web/src/pages/rss.xml.ts
@@ -1,14 +1,15 @@
+import type { APIContext } from "astro";
 import { getCollection } from "astro:content";
 
 import rss from "@astrojs/rss";
 
-export async function GET(context) {
+export async function GET(context: APIContext) {
   const blog = await getCollection("blog");
   return rss({
     title: "Somraj Saha’s Blog",
     description:
       "Somraj Saha's digital garden where he shares his knowledge, experience and learnings as a self-taught programmer.",
-    site: context.site,
+    site: context.site as URL,
     stylesheet: "/rss/styles.xsl",
     items: blog.map((post) => ({
       title: post.data.title,
