3faa0ef4e66cdda7a54081c3289133b6b5a8cfcc
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2023-12-21 15:18:47 +0530

    chore: add a bunch variables for Terraform to utilise

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,3 +4,31 @@ variable "vercel_api_token" {
   nullable    = false
   sensitive   = true
 }
+
+variable "vercel_project_name" {
+  type        = string
+  description = "The name of the project to showcase on the Vercel dashboard"
+  nullable    = false
+  default     = "personal-website"
+}
+
+variable "domain_name" {
+  type        = string
+  description = "The domain name to assign to the project when it is deployed to production"
+  nullable    = false
+  default     = "astro-jarmos.vercel.app"
+}
+
+variable "framework_name" {
+  type        = string
+  description = "The framework used to develop the project, examples are: Next.js, Hugo, Astro and so on"
+  nullable    = false
+  default     = "astro"
+}
+
+variable "project_source" {
+  type        = string
+  description = "The path on the filesystem to the project's source code"
+  nullable    = false
+  default     = "../web"
+}
