433a94e387481b6676b39c9afc450fdbbfe2fa53
Somraj Saha <somraj.saha@jarmos.dev>
2024-09-14 19:13:35 +0530

    chore: add a rudimentary hamburger menu which is interactive

---
+++ web/src/components/HamburgerMenu.vue
@@ -0,0 +1,21 @@
+<template>
+  <button role="button" @click="toggleMenu()">
+    <img src="/assets/menu.svg" alt="" height="50" />
+  </button>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const isMenuOpen = ref<boolean>(false);
+
+const toggleMenu = () => {
+  isMenuOpen.value = !isMenuOpen.value;
+};
+</script>
+
+<style scoped lang="scss">
+button {
+  cursor: pointer;
+}
+</style>
