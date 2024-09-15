9da601fbcb79c22a8ce0813065cd015319222fba
Somraj Saha <somraj.saha@jarmos.dev>
2024-09-15 15:33:51 +0530

    chore: add nav links to the modal menu

--- web/src/components/CommandPalette.vue
+++ web/src/components/CommandPalette.vue
@@ -1,11 +1,18 @@
 <template>
   <div v-if="show" class="modal-mask">
     <div class="modal-container">
-      <h2>Navigation Menu</h2>
-      <div>Close Button</div>
-      <div>Home</div>
-      <div>About</div>
-      <div>Blog</div>
+      <div class="container-header">
+        <h2>Navigation Menu</h2>
+        <button type="button">
+          <img src="/assets/close-menu.svg" alt="" height="25" />
+        </button>
+      </div>
+
+      <div class="nav-links">
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/blog">Blog</a>
+      </div>
     </div>
   </div>
 </template>
@@ -32,10 +39,38 @@ defineProps<{
     width: 500px;
     margin: auto;
     padding: 20px 30px;
-    background-color: #5e8ca7;
+    background-color: #243b4a;
     border-radius: 2px;
-    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+    box-shadow: 0 2px 8px rgba($color: #82f9a1, $alpha: 0.33);
     transition: all 0.3s ease;
+    border: solid 5px #82f9a1;
+
+    .container-header {
+      display: flex;
+      justify-content: space-between;
+      margin-bottom: 1rem;
+
+      h2 {
+        color: #82f9a1;
+      }
+    }
+
+    .nav-links {
+      display: flex;
+      flex-direction: column;
+      gap: 10px;
+
+      a {
+        text-decoration: none;
+        color: #c9e2f0;
+
+        &:hover {
+          color: #82f9a1;
+          text-decoration: underline;
+          text-underline-offset: 5px;
+        }
+      }
+    }
   }
 }
 </style>
