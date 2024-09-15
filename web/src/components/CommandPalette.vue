687434eaaf6015758de64f2f49f1f40c4ed5ec14
Somraj Saha <somraj.saha@jarmos.dev>
2024-09-15 14:52:47 +0530

    chore: add a baseline experimental modal

---
+++ web/src/components/CommandPalette.vue
@@ -0,0 +1,41 @@
+<template>
+  <div v-if="show" class="modal-mask">
+    <div class="modal-container">
+      <h2>Command Palette</h2>
+      <div>Close Button</div>
+      <div>Home</div>
+      <div>About</div>
+      <div>Blog</div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  show: Boolean;
+}>();
+</script>
+
+<style scoped lang="scss">
+.modal-mask {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  transition: opacity 0.3s ease;
+
+  .modal-container {
+    width: 500px;
+    margin: auto;
+    padding: 20px 30px;
+    background-color: #5e8ca7;
+    border-radius: 2px;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+    transition: all 0.3s ease;
+  }
+}
+</style>
--- web/src/components/HamburgerMenu.vue
+++ web/src/components/HamburgerMenu.vue
@@ -2,15 +2,21 @@
   <button role="button" @click="toggleMenu()">
     <img src="/assets/menu.svg" alt="" height="50" />
   </button>
+  <Teleport to="body">
+    <CommandPalette :show="isMenuOpen" @close="toggleMenu" />
+  </Teleport>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
 
+import CommandPalette from "./CommandPalette.vue";
+
 const isMenuOpen = ref<boolean>(false);
 
 const toggleMenu = () => {
   isMenuOpen.value = !isMenuOpen.value;
+  console.log(isMenuOpen.value);
 };
 </script>
 
