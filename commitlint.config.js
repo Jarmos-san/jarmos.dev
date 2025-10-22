dee3e91328b8f2b2ce11e868535a616d03d77b0d
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2025-10-22 16:51:25 +0530

    chore: remove all artifacts from the old version

--- commitlint.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: ["@commitlint/config-conventional"],
-  rules: {
-    "header-max-length": [2, "always", 72],
-    "body-max-line-length": [1, "always", Infinity],
-  },
-};
