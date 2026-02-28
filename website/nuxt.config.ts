5cd5d76ba8efcd5949ff6178c76070dbb3f2d96c
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2026-02-28 17:26:39 +0530

    chore: enable syntax highlighting for Go code

--- website/nuxt.config.ts
+++ website/nuxt.config.ts
@@ -24,7 +24,7 @@ export default defineNuxtConfig({
       markdown: {
         highlight: {
           theme: "everforest-dark",
-          langs: ["python", "javascript", "typescript", "shell", "lua"],
+          langs: ["python", "javascript", "typescript", "shell", "lua", "go"],
         },
       },
     },
